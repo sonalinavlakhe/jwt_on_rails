44a7720e169a3dbf5f97d7d35cfebbe4296098df
sonalinavlakhe <sonalibhavsar977@gmail.com>
2019-07-15 17:21:36 +0530

    Add routes specs for user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: :create do
-  	post 'confirm'
-  	post 'login'
-  	put 'update'
-  	get 'show'
-  end
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :users, only: [:create, :update, :show] do
+		post 'confirm'
+		post 'login'
+	end
 end
